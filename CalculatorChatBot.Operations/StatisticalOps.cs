1c0e20a2e748e67af5b36f90e594cb6c7f8ef367
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-12-13 17:56:09 -0800

    Update StatisticalOps.cs
    All of the code is complete - now to have the testing to begin

--- CalculatorChatBot.Operations/StatisticalOps.cs
+++ CalculatorChatBot.Operations/StatisticalOps.cs
@@ -2,7 +2,8 @@
 {
     using System;
     using System.Text;
-    using System.Collections.Generic; 
+    using System.Collections.Generic;
+    using System.Linq;
 
     public class StatisticalOps
     {
@@ -11,7 +12,7 @@
         /// </summary>
         /// <param name="inputString">Comma separated list of numbers</param>
         /// <returns>The mean (or average) of the list</returns>
-        public decimal Average(string inputString)
+        public decimal CalculateAverage(string inputString)
         {
             string[] inputStringArr = inputString.Split(',');
             int[] inputIntsArr = Array.ConvertAll(inputStringArr, int.Parse);
@@ -26,6 +27,67 @@
             var average = Convert.ToDecimal(sum) / sizeOfArray;
 
             return decimal.Round(average, 2);
+        }
+
+        public decimal CalculateMedian(string inputString)
+        {
+            string[] inputStringArr = inputString.Split(',');
+            int[] inputIntsArr = Array.ConvertAll(inputStringArr, int.Parse);
+
+            decimal median;
+            int size = inputIntsArr.Length;
+            int[] copyArr = inputIntsArr;
+
+            Array.Sort(copyArr);
+
+            if (size % 2 == 0)
+            {
+                median = Convert.ToDecimal(copyArr[size / 2 - 1] + copyArr[size / 2]) / 2;
+            }
+            else
+            {
+                median = Convert.ToDecimal(copyArr[(size - 1) / 2]);
+            }
+
+            return decimal.Round(median, 2);
+        }
+
+        public int[] CalculateMode(string inputString)
+        {
+            // From string to int array
+            string[] inputStringArr = inputString.Split(',');
+            int[] inputIntArr = Array.ConvertAll(inputStringArr, int.Parse);
+
+            // originalList is the original list of numbers
+            List<int> originalList = new List<int>(inputIntArr);
+
+            // This is what will be returned - modes.ToArray();
+            List<int> modes = new List<int>();
+
+            // The below code will have the data showing 
+            // each element and how many times each element appears in the list
+            var query = from numbers in originalList
+                        group numbers by numbers
+                            into groupedNumbers
+                        select new
+                        {
+                            Number = groupedNumbers.Key,
+                            Count = groupedNumbers.Count()
+                        };
+
+            int max = query.Max(g => g.Count);
+
+            if (max == 1)
+            {
+                int mode = 0;
+                modes.Add(mode); 
+            }
+            else
+            {
+                modes = query.Where(x => x.Count == max).Select(x => x.Number).ToList(); 
+            }
+
+            return modes.ToArray();
         }
     }
 }
