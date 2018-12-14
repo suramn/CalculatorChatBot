1379cfeb1f46177fc878c91edb02bff55e28c64b
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-12-13 21:46:46 -0800

    Adding the first unit test class and methods

--- CalculatorChatBot.Operations.Tests/ArithmeticTests.cs
+++ CalculatorChatBot.Operations.Tests/ArithmeticTests.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace CalculatorChatBot.Operations.Tests
 {
@@ -7,8 +7,20 @@ namespace CalculatorChatBot.Operations.Tests
     public class ArithmeticTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void OverallSummationTest()
         {
-        }
+            var arith = new ArithmeticOps();
+
+            var expSum = arith.OverallSum("1,3,4");
+            int testSum = 8;
+
+            Assert.AreEqual(expSum, testSum);
+
+            if (testSum == expSum)
+            {
+                Console.Write($"Expected overall sum: {expSum}; Test overall sum: {testSum}" + Environment.NewLine);
+                Console.Write("The OverallSummation test has passed");
+            }
+        } 
     }
 }
