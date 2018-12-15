b8fa556d29a36bd550af52f2dc7867932ac9b294
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-12-14 17:35:07 -0800

    Renaming files
    Also making sure that all of the changes are done and committed before I create the v2 branch of the Calculator Chat Bot

---
+++ CalculatorChatBot.Operations.Tests/StatisticalTests.cs
@@ -0,0 +1,26 @@
+﻿namespace CalculatorChatBot.Operations.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class StatisticalTests
+    {
+        [TestMethod]
+        public void AverageTest()
+        {
+            var stats = new StatisticalOps();
+            string inputStr = "1,2,3";
+            decimal testMean = 2;
+
+            var expectedMean = stats.CalculateAverage(inputStr);
+            Assert.AreEqual(testMean, expectedMean);
+
+            if (testMean == expectedMean)
+            {
+                Console.Write($"Expected Mean - {expectedMean}, Test Mean - {testMean}" + Environment.NewLine);
+                Console.Write("The AverageTest calculations pass");
+            }
+        }
+    }
+}
