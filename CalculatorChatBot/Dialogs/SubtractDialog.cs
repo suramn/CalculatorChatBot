4d267b0988718b8f43093550658542b629b4083c
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-12-16 18:58:40 -0800

    Update SubtractDialog.cs
    Having necessary properties added

--- CalculatorChatBot/Dialogs/SubtractDialog.cs
+++ CalculatorChatBot/Dialogs/SubtractDialog.cs
@@ -1,5 +1,6 @@
 ﻿namespace CalculatorChatBot.Dialogs
 {
+    using System;
     using System.Threading.Tasks;
     using Microsoft.Bot.Builder.Dialogs;
     using Microsoft.Bot.Connector;
@@ -7,12 +8,28 @@
     public class SubtractDialog : IDialog<object>
     {
         #region Dialog properties
+        public string InputString { get; set; }
 
+        public string[] InputStringArray { get; set; }
+
+        public int[] InputInts { get; set; }
         #endregion
 
         public SubtractDialog(Activity incomingActivity)
         {
+            // Extract the incoming text/message
+            string[] incomingInfo = incomingActivity.Text.Split(' ');
 
+            // What is the properties to be set for the necessary 
+            // operation to be performed
+            if (!string.IsNullOrEmpty(incomingInfo[1]))
+            {
+                InputString = incomingInfo[1];
+
+                InputStringArray = InputString.Split(',');
+
+                InputInts = Array.ConvertAll(InputStringArray, int.Parse);
+            }
         }
 
         public Task StartAsync(IDialogContext context)
