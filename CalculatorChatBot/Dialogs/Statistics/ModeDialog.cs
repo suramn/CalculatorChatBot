30988b206058c06521d5c936b360776fa41e5e2c
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-12-18 14:08:50 -0800

    Update ModeDialog.cs
    Having the Mode Calculations done

--- CalculatorChatBot/Dialogs/Statistics/ModeDialog.cs
+++ CalculatorChatBot/Dialogs/Statistics/ModeDialog.cs
@@ -4,6 +4,8 @@
     using Microsoft.Bot.Connector;
     using System;
     using System.Threading.Tasks;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public class ModeDialog : IDialog<object>
     {
@@ -32,7 +34,42 @@
 
         public async Task StartAsync(IDialogContext context)
         {
-            await context.PostAsync("Must populate the necessary functionality");
+            // Again making sure to conduct the appropriate things
+            if (InputInts.Length > 2)
+            {
+                List<int> originalList = new List<int>(InputInts);
+                List<int> modesList = new List<int>();
+
+                // Using LINQ in the lines below
+                var query = from numbers in originalList
+                            group numbers by numbers
+                                into groupedNumbers
+                            select new
+                            {
+                                Number = groupedNumbers.Key,
+                                Count = groupedNumbers.Count()
+                            };
+
+                int max = query.Max(g => g.Count);
+
+                if (max == 1)
+                {
+                    int mode = 0;
+                    modesList.Add(mode); 
+                }
+                else
+                {
+                    modesList = query.Where(x => x.Count == max).Select(x => x.Number).ToList();
+                }
+
+                var outputArray = modesList.ToArray().ToString();
+
+                await context.PostAsync($"Given the list: {InputString}; the mode = {outputArray}");
+            }
+            else
+            {
+                await context.PostAsync($"Please check your input list: {InputString} and try again later");
+            }
 
             // Returning back to the RootDialog - popping this child dialog off the stack
             context.Done<object>(null);
