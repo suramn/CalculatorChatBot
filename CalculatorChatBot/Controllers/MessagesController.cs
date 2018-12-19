978f6e33a84eea4c1c0af076fa24727bcd9025df
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-12-19 12:33:58 -0800

    Having some type of proactive messages enabled
    Now when the bot gets added to the channel, there is a proactive message that is now sent out

--- CalculatorChatBot/Controllers/MessagesController.cs
+++ CalculatorChatBot/Controllers/MessagesController.cs
@@ -1,11 +1,17 @@
 ﻿namespace CalculatorChatBot
 {
+    using CalculatorChatBot.BotHelpers;
     using CalculatorChatBot.BotMiddleware;
     using CalculatorChatBot.Dialogs;
     using Microsoft.Bot.Builder.Dialogs;
     using Microsoft.Bot.Connector;
+    using Microsoft.Bot.Connector.Teams;
+    using Microsoft.Bot.Connector.Teams.Models;
+    using Polly;
     using System;
+    using System.Collections.Generic;
     using System.Diagnostics;
+    using System.Linq;
     using System.Threading.Tasks;
     using System.Web.Http;
 
@@ -58,11 +64,57 @@
 
         private async Task<IHttpActionResult> HandleSystemMessageAsync(Activity message)
         {
-            if (message.Type == ActivityTypes.ConversationUpdate)
+            TeamEventBase eventData = message.GetConversationUpdateData();
+            switch (eventData.EventType)
             {
-            }
-            else if (message.Type == ActivityTypes.MessageReaction)
-            {
+                case TeamEventType.MembersAdded:
+                    var connector = new ConnectorClient(new Uri(message.ServiceUrl));
+                    connector.SetRetryPolicy(
+                        RetryHelpers.DefaultPolicyBuilder.WaitAndRetryAsync(new[] {
+                            TimeSpan.FromSeconds(2),
+                            TimeSpan.FromSeconds(5),
+                            TimeSpan.FromSeconds(10)
+                        }));
+
+                    var tenantId = message.GetTenantId();
+                    var botAccount = message.Recipient;
+                    var channelData = message.GetChannelData<TeamsChannelData>();
+
+                    // If the bot is in the collection of added members, then send the welcome message
+                    // to all team members
+                    if (message.MembersAdded.Any(m => m.Id.Equals(botAccount.Id)))
+                    {
+                        // Fetch members in the current conversation
+                        IList<ChannelAccount> channelAccount = await connector.Conversations.GetConversationMembersAsync(message.Conversation.Id);
+                        IEnumerable<TeamsChannelAccount> members = channelAccount.AsTeamsChannelAccounts();
+
+                        // Send a OneToOne message to each other
+                        foreach (TeamsChannelAccount member in members)
+                        {
+                            await MessageHelpers.SendOneToOneWelcomeMessage(connector, channelData, botAccount, member, tenantId);
+                        }
+                    }
+                    else
+                    {
+                        // Send the OneToOne message to new members
+                        foreach (TeamsChannelAccount member in message.MembersAdded.AsTeamsChannelAccounts())
+                        {
+                            await MessageHelpers.SendOneToOneWelcomeMessage(connector, channelData, botAccount, member, tenantId);
+                        }
+                    }
+                    break;
+                case TeamEventType.MembersRemoved:
+                    break;
+                case TeamEventType.ChannelCreated:
+                    break;
+                case TeamEventType.ChannelDeleted:
+                    break;
+                case TeamEventType.ChannelRenamed:
+                    break;
+                case TeamEventType.TeamRenamed:
+                    break;
+                default:
+                    break;
             }
 
             return Ok();
