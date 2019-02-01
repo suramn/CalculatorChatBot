3887b8ad7be76dfb1d26d4fde962c659d87b30f5
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-02-01 13:03:12 -0800

    Update GeometricOps.cs
    Updating and completing the method for the discriminant to be calculated accordingly

--- CalculatorChatBot.Operations/GeometricOps.cs
+++ CalculatorChatBot.Operations/GeometricOps.cs
@@ -1,10 +1,21 @@
 ﻿namespace CalculatorChatBot.Operations
 {
     using System;
-    using System.Collections.Generic;
-    using System.Text;
 
     public class GeometricOps
     {
+        public int CalculateDiscriminant(string inputString)
+        {
+            string[] inputStringArr = inputString.Split(',');
+            int[] inputIntsArr = Array.ConvertAll(inputStringArr, int.Parse);
+
+            int a = inputIntsArr[0];
+            int b = inputIntsArr[1];
+            int c = inputIntsArr[2];
+
+            int discriminant = (int)Math.Pow(b, 2) - (4 * a * c);
+
+            return discriminant;
+        }
     }
 }
