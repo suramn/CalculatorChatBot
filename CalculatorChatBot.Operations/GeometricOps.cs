21370a49ec1e79691f1166772a7e838263714c6a
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-02-06 13:28:19 -0800

    Update GeometricOps.cs
    Adding the necessary methods for testing later on

--- CalculatorChatBot.Operations/GeometricOps.cs
+++ CalculatorChatBot.Operations/GeometricOps.cs
@@ -17,5 +17,27 @@
 
             return discriminant;
         }
+
+        private int CalculateDiscriminant(int a, int b, int c)
+        {
+            var result = Math.Pow(b, 2) - (4 * a * c);
+            return (int)result;
+        }
+
+        public string CalculateQuadraticRoots(string inputString)
+        {
+            string[] inputStringArr = inputString.Split(',');
+            int[] inputIntsArr = Array.ConvertAll(inputStringArr, int.Parse);
+
+            var resultString = "";
+
+            return resultString; 
+        }
+
+        public string CalculatePythagoreanTriple(string inputString)
+        {
+            var resultString = "bleh";
+            return resultString; 
+        }
     }
 }
