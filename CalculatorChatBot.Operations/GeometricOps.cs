a760272f9ff7a7037e4465cc945c071d0fc3fd3b
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-02-13 14:34:40 -0800

    Adding more functionality to the GeometricOps.cs class

--- CalculatorChatBot.Operations/GeometricOps.cs
+++ CalculatorChatBot.Operations/GeometricOps.cs
@@ -35,9 +35,51 @@
                 int b = inputIntsArr[1];
                 int c = inputIntsArr[2];
 
-                // TODO: Make sure to complete this here
-                // 1. Have the necessary logic to calculate the discriminant
-                // 2. Have the necessary logic to use the switch mechanism to return the roots
+                double r1, r2, discriminant;
+                int m;
+
+                discriminant = Math.Pow(b, 2) - (4 * a * c);
+
+                if (a == 0)
+                {
+                    m = 1;
+                }
+                else if (discriminant > 0)
+                {
+                    m = 2;
+                }
+                else if (discriminant == 0)
+                {
+                    m = 3;
+                }
+                else
+                {
+                    m = 4;
+                }
+
+                switch (m)
+                {
+                    case 1:
+                        resultString = "Not a quadratic equation, linear equation";
+                        break;
+                    case 2:
+                        r1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                        r2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                        resultString = $"{r1.ToString()}, {r2.ToString()}";
+                        break;
+                    case 3:
+                        r1 = r2 = (-b) / (2 * a);
+                        resultString = r1.ToString();
+                        break;
+                    case 4:
+                        r1 = (-b) / (2 * a);
+                        r2 = Math.Sqrt(-discriminant) / (2 * a);
+                        resultString = string.Format("{0:#.##} + i {1:#.##}", r1, r2) + "," + string.Format("{0:#.##} - i {1:#.##}", r1, r2);
+                        break;
+                    default:
+                        resultString = "ERROR";
+                        break;
+                }
             }
 
             return resultString; 
