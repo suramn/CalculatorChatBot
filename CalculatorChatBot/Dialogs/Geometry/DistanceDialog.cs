a45ec2339ce975e9c8a9b2a0f9d14478e487f7ef
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-04-15 13:22:11 -0700

    More changes being made
    Adding the necessary attributes to the class

--- CalculatorChatBot/Dialogs/Geometry/DistanceDialog.cs
+++ CalculatorChatBot/Dialogs/Geometry/DistanceDialog.cs
@@ -5,6 +5,7 @@
     using System;
     using System.Threading.Tasks;
 
+    [Serializable]
     public class DistanceDialog : IDialog<object>
     {
         #region Dialog properties
--- CalculatorChatBot/Dialogs/Statistics/GeometricMeanDialog.cs
+++ CalculatorChatBot/Dialogs/Statistics/GeometricMeanDialog.cs
@@ -8,6 +8,7 @@
     using System.Collections.Generic;
     using System.Threading.Tasks;
 
+    [Serializable]
     public class GeometricMeanDialog : IDialog<object>
     {
         #region Dialog properties
