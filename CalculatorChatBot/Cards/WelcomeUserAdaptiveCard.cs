505eb47da6bce7fe03257d2294c7c492d29b3eac
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-05-10 12:29:35 -0700

    Welcome User Adaptive Card - ensuring to have things set up

---
+++ CalculatorChatBot/Cards/WelcomeUserAdaptiveCard.cs
@@ -0,0 +1,42 @@
+﻿namespace CalculatorChatBot.Cards
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Hosting;
+
+    public class WelcomeUserAdaptiveCard
+    {
+        private static readonly string CardTemplate;
+
+        static WelcomeUserAdaptiveCard()
+        {
+            var cardJsonFilePath = HostingEnvironment.MapPath("~/Cards/WelcomeUserAdaptiveCard.json");
+            CardTemplate = File.ReadAllText(cardJsonFilePath);
+        }
+
+        public static string GetCard()
+        {
+            var welcomeUserCardTitleText = "";
+            var welcomeUserCardIntroPart1 = "";
+            var welcomeUserCardIntroPart2 = "";
+
+            var variablesToValues = new Dictionary<string, string>()
+            {
+                { "welcomeUserCardTitleText", welcomeUserCardTitleText },
+                { "welcomeUserCardIntroPart1", welcomeUserCardIntroPart1 },
+                { "welcomeUserCardIntroPart2", welcomeUserCardIntroPart2 }
+            };
+
+            var cardBody = CardTemplate;
+            foreach (var kvp in variablesToValues)
+            {
+                cardBody = cardBody.Replace($"%{kvp.Key}%", kvp.Value);
+            }
+
+            return cardBody;
+        }
+    }
+}
