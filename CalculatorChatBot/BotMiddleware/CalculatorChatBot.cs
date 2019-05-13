3faab8c4137cbe83413cee05407e9e7734e85498
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-05-13 12:48:46 -0700

    Adding the CalculatorChatBot.cs class

---
+++ CalculatorChatBot/BotMiddleware/CalculatorChatBot.cs
@@ -0,0 +1,11 @@
+﻿namespace CalculatorChatBot.BotMiddleware
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class CalculatorChatBot
+    {
+    }
+}
