515310cc6c1f5a1cef874bb15231a0e90d876fb7
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-06-04 19:38:31 -0700

    Making the changes required to calculate the area of a trapezoid

---
+++ CalculatorChatBot/Dialogs/Geometry/TrapezoidAreaDialog.cs
@@ -0,0 +1,64 @@
+﻿namespace CalculatorChatBot.Dialogs.Geometry
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using CalculatorChatBot.Cards;
+    using CalculatorChatBot.Models;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Connector;
+
+    public class TrapezoidAreaDialog : IDialog<object>
+    {
+        #region Dialog properties
+        public string[] InputStringArray { get; set; }
+
+        public string InputString { get; set; }
+
+        public int[] InputInts { get; set; }
+        #endregion
+
+        public TrapezoidAreaDialog(Activity incomingActivity)
+        {
+            // Parsing through the incoming information
+            string[] incomingInfo = incomingActivity.Text.Split(' ');
+
+            // Setting all of the properties
+            if (!string.IsNullOrEmpty(incomingInfo[1]))
+            {
+                InputString = incomingInfo[1];
+                InputStringArray = InputString.Split(',');
+                InputInts = Array.ConvertAll(InputStringArray, int.Parse);
+            }
+        }
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            var operationType = CalculationTypes.Geometric;
+            if (InputInts.Length != 4)
+            {
+                var errorResultType = ResultTypes.Error;
+                var errorResults = new OperationResults()
+                {
+                    Input = InputString,
+                    NumericalResult = "0",
+                    OutputMsg = $"The input list: {InputString} may not be valid. Please try again",
+                    OperationType = operationType.GetDescription(),
+                    ResultType = errorResultType.GetDescription()
+                };
+
+                IMessageActivity errorReply = context.MakeMessage();
+                var errorAdaptiveCard = OperationErrorAdaptiveCard.GetCard(errorResults);
+                errorReply.Attachments = new List<Attachment>()
+                {
+
+                };
+            }
+        }
+    }
+}
